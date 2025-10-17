985d34e75ea4987e3a2955638c28f127ab4629ab
Abinash Karmakar <codeAbinash@gmail.com>
2025-10-17 11:23:42 +0000

    feat: Switch main application component from App to HPApp

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 
 import { AppRegistry, Text, TextInput } from 'react-native'
 import { name as appName } from './app.json'
-// import HPApp from './HPApp'
-import App from './App'
+import HPApp from './HPApp'
+// import App from './App'
 
 Text.defaultProps = Text.defaultProps || {}
 Text.defaultProps.allowFontScaling = true
 TextInput.defaultProps = TextInput.defaultProps || {}
 TextInput.defaultProps.allowFontScaling = false
 
-// AppRegistry.registerComponent(appName, () => HPApp)
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => HPApp)
+// AppRegistry.registerComponent(appName, () => App)
